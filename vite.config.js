df56989eacf0b257eb8e525326778aa227141786
Sara Raz <saralwraz@gmail.com>
2025-02-04 13:02:28 +0200

    Ammended url

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/moodmusic/",
+  base: "/musicmood/",
   plugins: [react()],
   server: {
     port: 5173,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
   },
 });
